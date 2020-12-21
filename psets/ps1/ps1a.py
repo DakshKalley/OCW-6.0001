d662a7c417d6a479cedb38b8b6065c57919a5414
Daksh Kalley <dakshk117@gmail.com>
2020-12-20 16:41:35 -0800

    completed solution to part a of pset1

--- psets/ps1/ps1a.py
+++ psets/ps1/ps1a.py
@@ -1,3 +1,6 @@
+# Part A: House Hunting
+# Daksh Kalley
+
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost = float(input("Enter the cost of your dream home: "))
